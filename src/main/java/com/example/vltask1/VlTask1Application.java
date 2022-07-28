e0267bb21c60d71a4121566ef69b469df26e1a45
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-07-28 14:48:57 +0200

    Start Task 1

---
+++ src/main/java/com/example/vltask1/VlTask1Application.java
@@ -0,0 +1,13 @@
+package com.example.vltask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlTask1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VlTask1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vltask1/VlTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vltask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VlTask1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
