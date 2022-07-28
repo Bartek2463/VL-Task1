f00d7b56f86abb2e233e02e947f4513b7f7ca061
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-07-28 15:43:37 +0200

    VLT 1.0 Create Model for User ,add notation lombook and  fields validation

---
+++ src/main/java/com/example/vltask1/model/User.java
@@ -0,0 +1,37 @@
+package com.example.vltask1.model;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Size(max = 45)
+    @Column(name = "USERNAME")
+    private String username;
+    @NotNull
+    @Size(max = 45)
+    @Column(name = "PASSWORD")
+    private String password;
+    @NotNull
+    @Size(max = 45)
+    @Column(name = "PERMISSION")
+    private String permission;
+    @NotNull
+    @Size(max = 45)
+    @Column(name = "READ_ONLY")
+    private String readOnly;
+}
